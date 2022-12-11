a00f39c7af6cb1f9a6321cf4f27fedc6fb3bc7db
Kalkidan Seboka <sebokaa.k@gmail.com>
2022-12-11 15:15:37 +0300

    add navigation component to gym page

--- src/pages/gym/Gym.jsx
+++ src/pages/gym/Gym.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Navbar } from '../../components'
 
 const Gym = () => {
-  return <div>Gym</div>
+  return (
+    <div className='relative w-full h-full'>
+      <Navbar />
+    </div>
+  )
 }
 
 export default Gym
