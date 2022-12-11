b3b70b5455cfc4862f2b4be74638e313c018dcef
Kalkidan Seboka <sebokaa.k@gmail.com>
2022-12-11 15:16:42 +0300

    add navigation export

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,4 @@
 export { default as HomeHeader } from './header/HomeHeader'
 export { default as LinkBtn } from './buttons/LinkBtn'
+export { default as Navbar } from './navigation/Navbar'
+export { default as NavLink } from './navigation/NavLink'
