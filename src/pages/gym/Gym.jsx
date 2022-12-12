3b1b2b23b5dc16efb794b6bf29a54ef9918a66d6
Kalkidan Seboka <sebokaa.k@gmail.com>
2022-12-12 18:16:48 +0300

    increase margin and padding

--- src/pages/gym/Gym.jsx
+++ src/pages/gym/Gym.jsx
@@ -1,9 +1,15 @@
+import { Card, Paper } from '@mantine/core'
 import React from 'react'
-import { Navbar } from '../../components'
+import { Navbar, DashHeader, Overview, RevenueOverview } from '../../components'
 
 const Gym = () => {
   return (
-    <div className='relative w-full h-full'>
+    <div className='relative w-full h-full px-5 py-7'>
+      <div className='md:ml-36 md:mt-8'>
+        <DashHeader who='Gym' />
+        <Overview />
+        <RevenueOverview />
+      </div>
       <Navbar />
     </div>
   )
