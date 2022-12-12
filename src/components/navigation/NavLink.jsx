260b5d551cde7203deff64b9345126e00b20797f
Kalkidan Seboka <sebokaa.k@gmail.com>
2022-12-12 14:56:47 +0300

    update margin and padding

--- src/components/navigation/NavLink.jsx
+++ src/components/navigation/NavLink.jsx
@@ -1,22 +1,28 @@
 import { useEffect, useState } from 'react'
 import { useViewportSize } from '@mantine/hooks'
 import { Tooltip, UnstyledButton } from '@mantine/core'
+import { Link } from 'react-router-dom'
 
-const NavLink = ({ label, icon }) => {
-  const [labelPosition, setLabelPosition] = useState('top')
+const NavLink = ({ label, icon, link }) => {
+  const [labelPosition, setLabelPosition] = useState('')
+  const [isDisabled, setIsDisabled] = useState(false)
   const { width } = useViewportSize()
 
   useEffect(() => {
-    if (width > 425) setLabelPosition('right')
-    else setLabelPosition('top')
+    // if (width < 425) setLabelPosition('')
+    // else setLabelPosition('right')
+    if (width < 425) setIsDisabled(true)
+    else setIsDisabled(false)
   }, [width])
 
   return (
-    <Tooltip label={label} position={labelPosition} transitionDuration={0}>
-      <UnstyledButton className='flex flex-col items-center justify-center gap-2 '>
-        {icon}
-        <span className='text-sm md:hidden'>{label}</span>
-      </UnstyledButton>
+    <Tooltip label={label} position='right' disabled={isDisabled} transitionDuration={0}>
+      <Link to={link}>
+        <UnstyledButton className='flex flex-col items-center justify-center gap-1'>
+          {icon}
+          <span className='text-sm md:hidden'>{label}</span>
+        </UnstyledButton>
+      </Link>
     </Tooltip>
   )
 }
--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,26 +1,30 @@
 import React from 'react'
 import { useViewportSize } from '@mantine/hooks'
 import { NavLink } from '../../components'
-import { IconHome2, IconUsers, IconFocusCentered, IconReceipt2, IconUserCircle } from '@tabler/icons'
-import { Paper } from '@mantine/core'
+import { IconHome2, IconUsers, IconFocusCentered, IconReceipt2, IconBarbell } from '@tabler/icons'
+import { Paper, Divider } from '@mantine/core'
 
 const Navbar = () => {
   const { width } = useViewportSize()
 
   return (
-    <div className='fixed bottom-0 left-0 w-full px-4 pb-4 md:top-0 md:h-full md:py-4'>
-      <Paper className='w-full px-6 py-3 bg-midnight md:h-full md:w-fit' radius='md' withBorder>
-        <div className='items-center justify-center hidden w-16 h-16 px-4 bg-white rounded-full md:flex'>
+    <div className='fixed bottom-0 left-0 w-full px-4 pb-4 md:top-0 md:h-full md:py-8'>
+      <Paper
+        className='w-full px-6 py-3 bg-midnight md:h-full md:w-fit md:flex md:flex-col md:justify-between md:items-center md:px-6'
+        radius='md'
+        withBorder
+      >
+        <div className='items-center justify-center hidden w-16 h-16 px-4 bg-white rounded-full md:flex mt-14'>
           <span className='text-black'>Nefi</span>
         </div>
-        <div className='flex justify-between gap-2 md:flex-col'>
-          <NavLink icon={<IconHome2 className='' />} label='Home' />
-          <NavLink icon={<IconFocusCentered className='' />} label='Scanner' />
-          <NavLink icon={<IconUsers className='' />} label='Members' />
-          <NavLink icon={<IconReceipt2 className='' />} label='Billing' />
+        <div className='flex justify-between gap-2 md:flex-col md:gap-12'>
+          <NavLink icon={<IconHome2 className='md:w-8 md:h-8' />} label='Home' link='/gym' />
+          <NavLink icon={<IconFocusCentered className='md:w-8 md:h-8' />} label='Scanner' link='/gym/checkin' />
+          <NavLink icon={<IconUsers className='md:w-8 md:h-8' />} label='Members' />
+          <NavLink icon={<IconReceipt2 className='md:w-8 md:h-8' />} label='Billing' />
         </div>
-        <div className='fixed top-0 right-0 flex p-3 mt-6 mr-4 border rounded-full bg-midnight border-neutral-700 md:static md:border-0 md:bg-inherit'>
-          <NavLink icon={<IconUsers className='w-6 h-6' />} label={width < 768 ? '' : 'Account'} />
+        <div className='fixed top-0 right-0 flex items-center justify-center p-3 mt-6 mr-4 border rounded-full bg-midnight border-neutral-700 md:static md:border-0 md:bg-inherit md:mr-0 md:mt-0 md:mb-6'>
+          <NavLink icon={<IconBarbell className='w-6 h-6 md:h-8 md:w-8' />} label={width < 768 ? '' : 'Account'} />
         </div>
       </Paper>
     </div>
@@ -28,3 +32,6 @@ const Navbar = () => {
 }
 
 export default Navbar
+
+// link='/gym/members'
+// link='/gym/billing'
