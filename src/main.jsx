6f9db8d0ffbdbf9dd509eed0a2faddc45bff432b
Kalkidan Seboka <sebokaa.k@gmail.com>
2022-12-16 18:08:09 +0300

    add supabase provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { MantineProvider } from '@mantine/core'
+import { Provider as SupabaseProvider } from 'react-supabase'
+import supabase from './utils/supabase'
+
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MantineProvider withCSSVariables withGlobalStyles withNormalizeCSS theme={{ colorScheme: 'dark' }}>
-      <App />
-    </MantineProvider>
+    <SupabaseProvider value={supabase}>
+      <MantineProvider withCSSVariables withGlobalStyles withNormalizeCSS theme={{ colorScheme: 'dark' }}>
+        <App />
+      </MantineProvider>
+    </SupabaseProvider>
   </React.StrictMode>
 )
