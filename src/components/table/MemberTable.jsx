e91994e1b4b3d299fbc032b045a1888929ce94e4
Kalkidan Seboka <sebokaa.k@gmail.com>
2022-12-16 18:12:54 +0300

    add member table component

--- src/components/table/MemberTable.jsx
+++ src/components/table/MemberTable.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import { useState } from 'react'
+import { Table } from '@mantine/core'
 
-const MemberTable = () => {
-  return <div>MemberTable</div>
+const MemberTable = ({ members }) => {
+  return (
+    <Table className='w-full h-full'>
+      <thead className='sticky top-0 py-6 bg-midnight'>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Membership</th>
+          {/* <th></th> */}
+        </tr>
+      </thead>
+      <tbody>
+        {members.map((member) => {
+          return (
+            <tr key={member.id}>
+              <td>{member.name}</td>
+              <td>{member.email}</td>
+              <td>{member.membership_type}</td>
+              {/* <td>{member.mass}</td> */}
+            </tr>
+          )
+        })}
+      </tbody>
+    </Table>
+  )
 }
 
+{
+  /* <h2 className='sticky top-0 text-center bg-midnight'>MemberList</h2> */
+}
 export default MemberTable
