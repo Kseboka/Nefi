78972d6876434e62a9cdfa5e9b15fa5ad550140e
Kalkidan Seboka <sebokaa.k@gmail.com>
2022-12-16 18:20:26 +0300

    remove layout

--- src/pages/gym/CheckIn.jsx
+++ src/pages/gym/CheckIn.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import QrReader from 'react-web-qr-reader'
 import { Layout, MemberCard } from '../../components'
 import { Paper } from '@mantine/core'
+import { useAuth } from '../../contexts/auth'
+import { Navigate, useNavigate } from 'react-router-dom'
 
 const CheckIn = () => {
   const previewStyle = {
@@ -10,25 +12,23 @@ const CheckIn = () => {
   }
 
   return (
-    <Layout header='Check-In'>
-      <Paper
-        className='flex flex-col w-full gap-4 p-4 overflow-hidden md:h-auto md:flex-row bg-midnight'
-        radius='md'
-        withBorder
-      >
-        <div className='overflow-hidden rounded-md md:h-auto md:w-2/5'>
-          <QrReader
-            delay={500}
-            resolution={200}
-            // onError={(error) => console.log(error)}
-            onScan={(result) => console.log(result)}
-          />
-        </div>
-        <div className='md:w-3/5'>
-          <MemberCard />
-        </div>
-      </Paper>
-    </Layout>
+    <Paper
+      className='flex flex-col w-full gap-4 p-4 overflow-hidden md:h-fit md:flex-row bg-midnight'
+      radius='md'
+      withBorder
+    >
+      <div className='overflow-hidden rounded-md md:h-auto md:w-3/5'>
+        <QrReader
+          delay={500}
+          resolution={200}
+          onError={(error) => console.log(error)}
+          onScan={(result) => console.log(result)}
+        />
+      </div>
+      <div className='md:w-3/5'>
+        <MemberCard />
+      </div>
+    </Paper>
   )
 }
 
