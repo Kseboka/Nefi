016243933ca71e8d6378925a56416bc22ac7a40a
Kalkidan Seboka <sebokaa.k@gmail.com>
2022-12-16 18:15:08 +0300

    add gym home

---
+++ src/pages/gym/GymHome.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Overview, RevenueOverview } from '../../components'
+
+const GymHome = () => {
+  return (
+    <div>
+      <Overview />
+      <RevenueOverview />
+    </div>
+  )
+}
+
+export default GymHome
