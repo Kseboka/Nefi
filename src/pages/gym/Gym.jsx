8125515381970b6c13812d3fe887129cec39bf81
Kalkidan Seboka <sebokaa.k@gmail.com>
2022-12-16 18:16:28 +0300

    Add gym home

--- src/pages/gym/Gym.jsx
+++ src/pages/gym/Gym.jsx
@@ -1,17 +1,14 @@
-import { Card, Paper } from '@mantine/core'
-import React from 'react'
-import { Navbar, DashHeader, Overview, RevenueOverview } from '../../components'
+import { useEffect, useState } from 'react'
+import supabase from '../../utils/supabase'
+import { Overview, RevenueOverview } from '../../components'
+import { Link, useParams, useNavigate, Outlet } from 'react-router-dom'
 
 const Gym = () => {
   return (
-    <div className='relative w-full h-full px-5 py-7'>
-      <div className='md:ml-36 md:mt-8'>
-        <DashHeader who='Gym' />
-        <Overview />
-        <RevenueOverview />
-      </div>
-      <Navbar />
-    </div>
+    <>
+      <Overview />
+      <RevenueOverview />
+    </>
   )
 }
 
