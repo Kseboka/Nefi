f4c9bc4fc1606a99e7d6ee0b556f2c50738e977a
Kalkidan Seboka <sebokaa.k@gmail.com>
2022-12-16 18:18:30 +0300

    update styling

--- src/components/overview/OverviewCard.jsx
+++ src/components/overview/OverviewCard.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Card } from '@mantine/core'
 
-const OverviewCard = ({ whatFor, bgColor, icon }) => {
+const OverviewCard = ({ whatFor, bgColor, icon, stat }) => {
   return (
     <Card className={` rounded-sm w-full ${bgColor} text-slate-100 flex items-center gap-8 shadow-md`}>
       {icon}
       <div>
-        <h3 className='font-black'>{whatFor}</h3>
-        <p>50</p>
+        <h2 className=' md:text-4xl md:font-bold'>{stat}</h2>
+        <p className='font-medium md:text-2xl'>{whatFor}</p>
       </div>
     </Card>
   )
