372145855cea210ea3cc3fd64166e27f06a7b863
Kalkidan Seboka <sebokaa.k@gmail.com>
2022-12-19 22:00:36 +0300

    deploy

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.VITE_SUPABASE_URL
+const supabaseUrl = process.env.VITE_PUBLIC_SUPABASE_UR
 const supabaseKey = process.env.VITE_SUPABASE_KEY
 
 const supabase = createClient(supabaseUrl, supabaseKey)
